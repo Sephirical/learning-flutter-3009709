a28805dcbccec534cb899ab19eca5191c8d4b4ae
Sephirical <jh.tran1998@gmail.com>
2023-02-14 23:09:29 +1100

    finished 04_12

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -11,7 +11,7 @@ class ChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Chat App",
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.deepPurple, appBarTheme: AppBarTheme(backgroundColor: Colors.blue, foregroundColor: Colors.black)),
       home: LoginPage(),
     );
   }
