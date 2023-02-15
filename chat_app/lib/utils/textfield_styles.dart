0801602a4046fee3ba98f74014fb94b263cbdf75
Sephirical <jh.tran1998@gmail.com>
2023-02-15 16:03:15 +1100

    finished 05_04

--- chat_app/lib/utils/textfield_styles.dart
+++ chat_app/lib/utils/textfield_styles.dart
@@ -1,6 +1,6 @@
-
+import "package:google_fonts/google_fonts.dart";
 import 'package:flutter/material.dart';
 
 class ThemeTextStyle{
-  static TextStyle loginTextFieldStyle = TextStyle(color: Colors.blueGrey);
+  static TextStyle loginTextFieldStyle = GoogleFonts.lato(textStyle: TextStyle(color: Colors.blueGrey));
 }
