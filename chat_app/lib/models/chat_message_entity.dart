8c7b7f07f16dcef6ea5d498caa0436b0fd39f04f
Sephirical <jh.tran1998@gmail.com>
2023-02-16 23:29:20 +1100

    finished 05_08

---
+++ chat_app/lib/models/chat_message_entity.dart
@@ -0,0 +1,16 @@
+
+class ChatMessageEntity {
+  String text;
+  String? imageUrl;
+  String id;
+  int createdAt;
+  Author author;
+
+  ChatMessageEntity({required this.text, required this.id, required this.createdAt, required this.author, this.imageUrl});
+}
+
+class Author {
+  String userName;
+
+  Author({required this.userName});
+}
