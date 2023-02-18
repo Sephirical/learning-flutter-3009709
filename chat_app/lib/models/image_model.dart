6fd652743e5b759e05277aa603af6fd45bdc0e9c
Sephirical <jh.tran1998@gmail.com>
2023-02-18 11:00:50 +1100

    finished 06_03

---
+++ chat_app/lib/models/image_model.dart
@@ -0,0 +1,20 @@
+
+import 'package:json_annotation/json_annotation.dart';
+part 'image_model.g.dart';
+
+@JsonSerializable()
+class PixelfordImage {
+  String id;
+  String filename;
+  String? title;
+
+  @JsonKey(name: 'url_full_size')
+  String urlFullSize;
+  @JsonKey(name: 'url_small_size')
+  String urlSmallSize;
+
+  PixelfordImage({ this.title, required this.filename, required this.urlFullSize, required this.urlSmallSize, required this.id });
+
+  factory PixelfordImage.fromJson(Map<String, dynamic> json) => _$PixelfordImageFromJson(json);
+
+}
